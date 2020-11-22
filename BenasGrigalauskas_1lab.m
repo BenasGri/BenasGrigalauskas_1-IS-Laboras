f7f5acab044d22af9c6baaaaa49a5bb73e3d1119
Benas Grigalauskas <73246949+BenasGri@users.noreply.github.com>
2020-11-22 19:01:01 +0200

    sutvarkytas atnaujinimas

--- BenasGrigalauskas_1lab.m
+++ BenasGrigalauskas_1lab.m
@@ -12,16 +12,18 @@ b = randn(1);
 x1 = data(:,1)';
 x2 = data(:,2)';
 T = data(:,3)';
-eta = 0.25;
+eta = 0.1;
 
-for i = 1:length(x1)
-    if (x1(i)*w1 + x2(i)*w2 + b) > 0
-        y(i) = 1;
-    else
-        y(i) = -1;
+for n = 1:20 
+    for i = 1:length(x1)
+        if (x1(i)*w1 + x2(i)*w2 + b) > 0
+            y(i) = 1;
+        else
+            y(i) = -1;
+        end
+        e(i) = T(i)-y(i);         %momentine klaida
+        w1 = w1 + eta*e(i)*x1(i);
+        w2 = w2 + eta*e(i)*x2(i);
+        b = b + eta*e(i);
     end
-    e(i) = Y(i)-y(i);         %momentine klaida
-    w1(i+1) = w1(i) + eta*e(i)*x1(i);
-    w2(i+1) = w2(i) + eta*e(i)*x2(i);
-    b(i+1) = b(i) + eta*e(i);
 end
